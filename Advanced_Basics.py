7cff2f82a3e240995ada04c62c958bcbcd676306
idh421 <idh421@yahoocom>
2021-12-04 11:38:32 -0500

    Chapters 1 to 3

---
+++ Advanced_Basics.py
@@ -0,0 +1,21 @@
+# Pro Python
+# Chapter 2: Advanced Basics
+
+# Conditional Expressions
+value = 50
+print('The value is ' + ('just right.' if value < 100 else 'too big!'))
+
+# Iteration
+print(range(5))
+
+# Sequence Unpacking
+x, y, z = (1, 2, 3)
+print(x)
+
+# List Comprehensions
+output = [str(value) for value in range(10) if value > 5]
+print(output)
+
+# Set Comprehensions
+
+
